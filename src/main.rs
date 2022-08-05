8c0c8317b0680acec21ef23a8544920c19708a68
Mominul <mominul2082@gmail.com>
2022-08-05 10:05:48 +0600

    login form

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,40 @@
-#[macro_use] extern crate rocket;
+
+
 use anyhow::Result;
-use rocket::State;
+use rocket::{State, form::FromForm, tokio::fs::read_to_string};
 // use sqlx::sqlite::SqlitePoolOptions;
 use sqlx::{mysql::MySqlPoolOptions, MySql, Pool};
+use rocket::{get, post, routes, launch};
+use rocket::form::Form;
 
-#[get("/login/<id>/<pass>")]
-async fn login(pool: &State<Pool<MySql>>, id: &str, pass: &str) -> String {
-    let receiv_pass = sqlx::query!("SELECT pass FROM admin WHERE id = ?",id).fetch_one(&**pool).await.unwrap();
-    if receiv_pass.pass == pass {
+#[derive(FromForm)]
+struct LogIn<'r> {
+    id: &'r str,
+    pass: &'r str,
+}
+
+#[post("/login", data = "<login>")]
+async fn login(pool: &State<Pool<MySql>>, login: Form<LogIn<'_>>) -> String {
+    let receiv_pass = sqlx::query!("SELECT pass FROM admin WHERE id = ?",login.id).fetch_one(&**pool).await.unwrap();
+    if receiv_pass.pass == login.pass {
         "Login Success".to_string()
-    } else {
+    }
+    else {
         "Access Denied".to_string()
     }
 }
 
 #[get("/")]
-async fn front() -> String {
-    format!("Front page")
+async fn front() -> rocket::response::content::RawHtml<String> {
+    let html = read_to_string("./template/sample.html").await.unwrap();
+    rocket::response::content::RawHtml(html)
 }
 
 #[launch]
 async fn run() -> _ {
     // et ldb = SqlitePoolOptions::new().connect("./data/data.db").await.unwrap();
     let db = MySqlPoolOptions::new().connect("mysql://root:@127.0.0.1/data").await.unwrap();
-    rocket::build().mount("/", routes![login])
+    rocket::build()//.mount("/", routes![login])
                    .mount("/", routes![front])
                    .manage(db)
 }
